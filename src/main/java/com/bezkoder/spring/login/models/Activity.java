246a898195cfbff9f155fd60d644bfacdd3c7c6d
Stefano De Ciantis <mikesh07mail@gmail.com>
2024-02-16 17:23:31 +0100

    initial definition of the activity table

--- src/main/java/com/bezkoder/spring/login/models/Activity.java
+++ src/main/java/com/bezkoder/spring/login/models/Activity.java
@@ -1,23 +1,115 @@
 package com.bezkoder.spring.login.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 // This tells Hibernate to make a table out of this class
 @Entity
+@Table(name = "attivita")
 public class Activity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    // ID attività
     private Integer id;
-    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "id_organizzatore")
+    private User organizzatore;
+
+    @Column(name = "data_inizio")
+    private Date dataInizio;
+
+    @Column(name = "data_fine")
+    private Date dataFine;
+
+    // Nome attività
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descrizione")
+    private String descrizione;
+
+    //TODO: per l'immagine di copertina, per il momento assumo che si usi un link esterno ad un altra risorsa
+    @Column(name = "img_copertina")
+    private String imgCopertina;
+
+    @Column(name = "latitudine")
+    private String latitudine;
+
+    @Column(name = "longitudine")
+    private String longitudine;
 
     public String getName() {
-        return name;
+        return nome;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.nome = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getOrganizzatore() {
+        return organizzatore;
+    }
+
+    public void setOrganizzatore(User organizzatore) {
+        this.organizzatore = organizzatore;
+    }
+
+    public Date getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(Date dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public Date getDataFine() {
+        return dataFine;
+    }
+
+    public void setDataFine(Date dataFine) {
+        this.dataFine = dataFine;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getImgCopertina() {
+        return imgCopertina;
+    }
+
+    public void setImgCopertina(String imgCopertina) {
+        this.imgCopertina = imgCopertina;
+    }
+
+    public String getLatitudine() {
+        return latitudine;
+    }
+
+    public void setLatitudine(String latitudine) {
+        this.latitudine = latitudine;
+    }
+
+    public String getLongitudine() {
+        return longitudine;
+    }
+
+    public void setLongitudine(String longitudine) {
+        this.longitudine = longitudine;
     }
 }
