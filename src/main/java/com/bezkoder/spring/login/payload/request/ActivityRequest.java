fb27661daf3f848348ec7bace644e905cdf4fc4c
Stefano De Ciantis <mikesh07mail@gmail.com>
2024-02-21 17:15:23 +0100

    Update ActivityRequest.java

--- src/main/java/com/bezkoder/spring/login/payload/request/ActivityRequest.java
+++ src/main/java/com/bezkoder/spring/login/payload/request/ActivityRequest.java
@@ -1,20 +1,53 @@
 package com.bezkoder.spring.login.payload.request;
 
+import java.util.Date;
 import java.util.Set;
 
 import jakarta.validation.constraints.*;
 
 public class ActivityRequest {
     @NotBlank
-    @Size(min = 3, max = 20)
-    private String username;
+    @Size(min = 3, max = 40)
+    private String nome;
 
-    public String getUsername() {
-        return username;
+    @NotBlank
+    private String descrizione;
+
+    @NotBlank
+    private Date dataFine;
+
+    @NotBlank
+    private Date dataInizio;
+
+    public String getNome() {
+        return nome;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Date getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(Date dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public Date getDataFine() {
+        return dataFine;
+    }
+
+    public void setDataFine(Date dataFine) {
+        this.dataFine = dataFine;
+    }
 }
