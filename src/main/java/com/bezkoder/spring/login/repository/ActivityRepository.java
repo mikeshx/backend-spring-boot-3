c335edebc745ced8b2ee20caef829e87c6314868
Stefano De Ciantis <mikesh07mail@gmail.com>
2024-02-21 17:17:38 +0100

    Update ActivityRepository.java

--- src/main/java/com/bezkoder/spring/login/repository/ActivityRepository.java
+++ src/main/java/com/bezkoder/spring/login/repository/ActivityRepository.java
@@ -1,8 +1,10 @@
 package com.bezkoder.spring.login.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.bezkoder.spring.login.models.Activity;
+import com.bezkoder.spring.login.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,7 @@ import com.bezkoder.spring.login.models.Role;
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
+    // Restituisce tutte le attività organizzate da un utente
+    // SELECT * FROM Activity WHERE id_organizzatore = 1;
+    List<Activity> findByOrganizzatoreId(Long organizzatoreId);
 }
